dfe49891d1510777ef785492a502fb29a50c7b88
Niklas Johansson <raphexion@gmail.com>
2019-03-30 18:34:03 +0100

    Adds property test for nested put_file

--- test/prop_file.erl
+++ test/prop_file.erl
@@ -5,18 +5,37 @@
 %%% Properties %%%
 %%%%%%%%%%%%%%%%%%
 prop_test() ->
-    {ok, Fs} = erlmemfs:start_link(),
-    ?FORALL({Name, Content}, {non_empty(string()), binary()},
+    ?FORALL({Name, Content}, {file(), content()},
 	    begin
+		{ok, Fs} = erlmemfs:start_link(),
 		combine(Fs, Name, Content, [fun put_file/3,
 					    fun get_file/3,
 					    fun del_file/3,
 					    fun gone/3])
 	    end).
 
+prop_nested() ->
+    ?FORALL({Folder, Name, Content}, {folder(), file(), content()},
+	    begin
+		{ok, Fs} = erlmemfs:start_link(),
+		make_path(Fs, Folder),
+		put_file(Fs, Name, Content)
+		    andalso get_file(Fs, Name, Content)
+		    andalso correct_count(Fs, Folder, 1)
+		    andalso del_file(Fs, Name, Content)
+		    andalso correct_count(Fs, Folder, 0)
+	    end).
+
 %%%%%%%%%%%%%%%
 %%% Helpers %%%
 %%%%%%%%%%%%%%%
+
+make_path(_Fs, []) ->
+    ok;
+make_path(Fs, [H|T]) ->
+    erlmemfs:make_directory(Fs, H),
+    erlmemfs:change_directory(Fs, H),
+    make_path(Fs, T).
 
 combine(_Fs, _Name, _Content, []) ->
     true;
@@ -35,3 +54,20 @@ del_file(Fs, Name, _) ->
 
 gone(Fs, Name, _) ->
     {error, missing_file} =:= erlmemfs:get_file(Fs, Name).
+
+correct_count(Fs, Folder, NbFiles) ->
+    N = length(Folder),
+    {ok, #{file => NbFiles, dir => N}} =:= erlmemfs:count(Fs).
+
+%%%%%%%%%%%%%%%%%%
+%%% Generators %%%
+%%%%%%%%%%%%%%%%%%
+
+folder() ->
+    non_empty(list(non_empty(string()))).
+
+file() ->
+    non_empty(string()).
+
+content() ->
+    non_empty(binary()).
