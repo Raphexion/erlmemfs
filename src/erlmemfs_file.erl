912220aa263611c9e59f6635fc2047ab2de53316
Niklas Johansson <raphexion@gmail.com>
2019-04-05 18:20:56 +0200

    Adds initial implementation of write_block

--- src/erlmemfs_file.erl
+++ src/erlmemfs_file.erl
@@ -7,6 +7,7 @@
 	 stop/1,
 	 open/1,
 	 close/2,
+	 write_block/3,
 	 read_block/3]).
 
 %% Behaviour callbacks
@@ -34,6 +35,9 @@ open(Pid) ->
 close(Pid, Ref) ->
     gen_server:call(Pid, {close, Ref}).
 
+write_block(Pid, Ref, Fun) ->
+    gen_server:call(Pid, {write_block, Ref, Fun}).
+
 read_block(Pid, Ref, NbBytes) ->
     gen_server:call(Pid, {read, Ref, NbBytes}).
 
@@ -42,12 +46,16 @@ read_block(Pid, Ref, NbBytes) ->
 %%------------------------------------------------------------------------------
 
 -record(state, {
-	  data = <<>> :: binary(),
-	  refs = #{} :: map()
-	 }).
+		data = <<>> :: binary(),
+		refs = #{} :: map(),
+		locked = false :: boolean()
+	       }).
 
 init(Data) ->
     {ok, #state{data=Data}}.
+
+handle_call(_What, _From, State=#state{locked=true}) ->
+    {reply, {error, locked}, State};
 
 handle_call(open, _From, State=#state{refs=Refs}) ->
     Ref = make_ref(),
@@ -61,6 +69,16 @@ handle_call({close, Ref}, _From, State=#state{refs=Refs}) ->
 	    {reply, {error, alread_closed}, State};
 	_Size ->
 	    {reply, {ok, closed}, State#state{refs=Refs#{Ref => closed}}}
+    end;
+
+handle_call({write_block, _Ref, Fun}, From, State=#state{refs=Refs}) ->
+    case maps:filter(fun(_, V) -> V /= closed end, Refs) of
+	[] ->
+	    erlang:send_after(0, self(), {write, Fun, From}),
+	    Empty = <<>>,
+	    {noreply, State#state{locked=true, data=Empty}};
+	_ ->
+	    {reply, {error, busy}, State}
     end;
 
 handle_call({read, Ref, NbBytes}, _From, State=#state{data=Data, refs=Refs}) ->
@@ -79,6 +97,9 @@ handle_call(What, _From, State) ->
 handle_cast(_What, State) ->
     {noreply, State}.
 
+handle_info({write, Fun, From}, State) ->
+    {noreply, priv_write_blocks(Fun, From, State)};
+
 handle_info(_What, State) ->
     {noreply, State}.
 
@@ -91,6 +112,18 @@ code_change(_OldVsn, State, _Extra) ->
 %%-----------------------------------------------------------------------------
 %% Behaviour callbacks
 %%------------------------------------------------------------------------------
+
+priv_write_blocks(Fun, From, State) ->
+    case Fun() of
+        {ok, Bytes, _ReadCount} ->
+	    erlang:send_after(0, self(), {write, Fun, From}),
+	    #state{data=Data} = State,
+	    NewData = <<Data/binary, Bytes/binary>>,
+	    State#state{data=NewData};
+        done ->
+	    gen_server:reply(From, done),
+	    State
+    end.
 
 priv_read(_Data, _NbBytes, badkey) ->
     badkey;
