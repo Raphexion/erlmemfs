afaf9bb788e10e2179ed18ac02276cd18f0c49d7
Niklas Johansson <raphexion@gmail.com>
2019-04-22 20:18:12 +0200

    Adds property test for support move up (extra)

--- test/prop_support.erl
+++ test/prop_support.erl
@@ -5,12 +5,21 @@
 %%%%%%%%%%%%%%%%%%
 %%% Properties %%%
 %%%%%%%%%%%%%%%%%%
-prop_test() ->
+prop_move_ups_test() ->
     ?FORALL(Folders, prop_generators:folders(),
 	    begin
 		{ok, Leaf} = erlmemfs_support:mkdirs(erlmemfs_support:root(), Folders),
 		NbSteps = length(Folders),
 		#dir{name=Name, parent=Parent} = erlmemfs_support:move_ups(Leaf, NbSteps),
+		Name =:= "/" andalso Parent =:= none
+	    end).
+
+prop_move_ups_past_test() ->
+    ?FORALL(Folders, prop_generators:folders(),
+	    begin
+		{ok, Leaf} = erlmemfs_support:mkdirs(erlmemfs_support:root(), Folders),
+		NbSteps = length(Folders),
+		#dir{name=Name, parent=Parent} = erlmemfs_support:move_ups(Leaf, NbSteps + 1),
 		Name =:= "/" andalso Parent =:= none
 	    end).
 
