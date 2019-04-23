c5b2c42ca1308cbb8666cb513622e00b2808c7bc
Niklas Johansson <raphexion@gmail.com>
2019-04-23 11:00:04 +0200

    Adds prop generator for unique files and folders

--- test/prop_generators.erl
+++ test/prop_generators.erl
@@ -5,7 +5,8 @@
 	 files/0,
 	 content/0,
 	 folder/0,
-	 folders/0]).
+	 folders/0,
+	 unique_files_and_folders/0]).
 
 file() ->
     non_empty(string()).
@@ -24,3 +25,12 @@ folders() ->
 
 invalid(Folder) ->
     lists:member($/, Folder) or (Folder =:= ".") or (Folder =:= "..").
+
+files_and_folders() ->
+    {files(), folders()}.
+
+unique_files_and_folders() ->
+    ?SUCHTHAT({Files, Folders}, files_and_folders(), unique_list(Files ++ Folders)).
+
+unique_list(List) ->
+    length(List) =:= sets:size(sets:from_list(List)).
