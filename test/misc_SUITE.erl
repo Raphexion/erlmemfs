3b6e37edbeb078fd832ca3d37a4c23bb71da9834
Niklas Johansson <raphexion@gmail.com>
2019-05-21 22:20:28 +0200

    Adds ct for close file with bad reference

--- test/misc_SUITE.erl
+++ test/misc_SUITE.erl
@@ -1,11 +1,19 @@
 -module(misc_SUITE).
 -include_lib("common_test/include/ct.hrl").
 -export([all/0]).
--export([test_erlmemfs_debug/1]).
+-export([test_erlmemfs_debug/1,
+	 test_close_file_with_bad_reference/1]).
 
-all() -> [test_erlmemfs_debug].
+all() -> [test_erlmemfs_debug,
+	  test_close_file_with_bad_reference].
 
 test_erlmemfs_debug(_Config) ->
     application:ensure_started(erlmemfs),
     {ok, Fs} = erlmemfs_sup:create_erlmemfs(),
     {ok, erlmemfs_support:root()} =:= erlmemfs:debug(Fs).
+
+test_close_file_with_bad_reference(_Config) ->
+    application:ensure_started(erlmemfs),
+    {ok, Fp} = erlmemfs_file_sup:create_erlmemfs_file(<<1,2,3,4>>),
+    BadRef = erlang:make_ref(),
+    {error, missing} = erlmemfs_file:close(Fp, BadRef).
