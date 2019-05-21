f0083d6a818ed5cc667479095a682b93dbd1984f
Niklas Johansson <raphexion@gmail.com>
2019-05-21 22:35:37 +0200

    Adds test for list single file

--- test/misc_SUITE.erl
+++ test/misc_SUITE.erl
@@ -3,11 +3,13 @@
 -export([all/0]).
 -export([test_erlmemfs_debug/1,
 	 test_close_file_with_bad_reference/1,
-	 test_list_of_missing_file/1]).
+	 test_list_of_missing_file/1,
+	 test_list_of_single_file/1]).
 
 all() -> [test_erlmemfs_debug,
 	  test_close_file_with_bad_reference,
-	  test_list_of_missing_file].
+	  test_list_of_missing_file,
+	  test_list_of_single_file].
 
 test_erlmemfs_debug(_Config) ->
     application:ensure_started(erlmemfs),
@@ -26,3 +28,11 @@ test_list_of_missing_file(_Config) ->
     {ok, Fs} = erlmemfs_sup:create_erlmemfs(),
     MissingFile = "abc",
     {error, missing} = erlmemfs:list_files(Fs, MissingFile).
+
+test_list_of_single_file(_Config) ->
+    application:ensure_started(erlmemfs),
+    {ok, Fs} = erlmemfs_sup:create_erlmemfs(),
+    Name = "my_file.txt",
+    Data = <<1,2,3,4>>,
+    {ok, Name} = erlmemfs:put_file(Fs, Name, Data),
+    {ok, [Name]} = erlmemfs:list_files(Fs, Name).
