f9574d10aac56f60377a71482ca07754aab55dad
Niklas Johansson <raphexion@gmail.com>
2019-05-21 22:32:41 +0200

    Adds ct test for list missing file

--- test/misc_SUITE.erl
+++ test/misc_SUITE.erl
@@ -2,10 +2,12 @@
 -include_lib("common_test/include/ct.hrl").
 -export([all/0]).
 -export([test_erlmemfs_debug/1,
-	 test_close_file_with_bad_reference/1]).
+	 test_close_file_with_bad_reference/1,
+	 test_list_of_missing_file/1]).
 
 all() -> [test_erlmemfs_debug,
-	  test_close_file_with_bad_reference].
+	  test_close_file_with_bad_reference,
+	  test_list_of_missing_file].
 
 test_erlmemfs_debug(_Config) ->
     application:ensure_started(erlmemfs),
@@ -18,3 +20,9 @@ test_close_file_with_bad_reference(_Config) ->
     {ok, Fp} = erlmemfs_file_sup:create_erlmemfs_file(<<1,2,3,4>>),
     BadRef = erlang:make_ref(),
     {error, missing} = erlmemfs_file:close(Fp, BadRef).
+
+test_list_of_missing_file(_Config) ->
+    application:ensure_started(erlmemfs),
+    {ok, Fs} = erlmemfs_sup:create_erlmemfs(),
+    MissingFile = "abc",
+    {error, missing} = erlmemfs:list_files(Fs, MissingFile).
